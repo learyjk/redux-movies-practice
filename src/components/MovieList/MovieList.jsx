3bdfbe1e8170f74ef19cb98c30c003c4a1571077
Keegan <leary.keegan@gmail.com>
2022-05-30 12:35:54 -0700

    setup

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieList = () => {
+  return (
+    <div>
+      Movie List
+    </div>
+  );
+};
+
+export default MovieList;
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      Page Note Found
+    </div>
+  );
+};
+
+export default PageNotFound;
